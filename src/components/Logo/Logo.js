008f04c5e5d14e0ec9514b1bab1537ba7a559d25
Chuazz <anhsonbeo97@gmail.com>
2023-02-17 09:41:48 +0700

    Using NavLink instead of using my custome context

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,8 +1,7 @@
 // Framework
 import propTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import { memo, useContext } from 'react';
-import { PageContext } from '@/contexts/pageProvider';
+import { memo } from 'react';
 import clsx from 'clsx';
 
 // Component
@@ -16,13 +15,11 @@ import { logo } from '@/assets/images';
 import routes from '@/config/routes';
 
 const Logo = ({ className }) => {
-    const context = useContext(PageContext);
-
     return (
         <div className={clsx(styles.container, className)}>
             <div className="row ali-center">
                 <Link to={routes.home} className="row ali-center">
-                    <Image src={logo} onClick={() => context.onClick('home')} />
+                    <Image src={logo} />
                 </Link>
                 <p className={clsx(styles.title)}>Mua bán và sửa chữa</p>
             </div>
