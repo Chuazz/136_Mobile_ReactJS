5195d87749bfb811e0478fa6354e1917cb5ae20e
Chuazz <anhsonbeo97@gmail.com>
2023-02-17 09:34:24 +0700

    Using NavLink instead of using my custome context

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 // Framework
 import { memo } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import PageProvider from './contexts/pageProvider';
 import DefaultLayout from './layouts/DefaultLayout';
 
 // Data
@@ -10,28 +9,26 @@ import { publicRoutes } from './routes';
 function App() {
     return (
         <Router>
-            <PageProvider>
-                <div className="App">
-                    <Routes>
-                        {publicRoutes.map((route, index) => {
-                            const Component = route.element;
-                            let Layout = DefaultLayout;
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        const Component = route.element;
+                        let Layout = DefaultLayout;
 
-                            return (
-                                <Route
-                                    key={index}
-                                    path={route.path}
-                                    element={
-                                        <Layout>
-                                            <Component />
-                                        </Layout>
-                                    }
-                                ></Route>
-                            );
-                        })}
-                    </Routes>
-                </div>
-            </PageProvider>
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Component />
+                                    </Layout>
+                                }
+                            ></Route>
+                        );
+                    })}
+                </Routes>
+            </div>
         </Router>
     );
 }
