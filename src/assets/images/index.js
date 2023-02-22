e67fe2534fb0d0881a853750b9a50609eed3a838
Chuazz <anhsonbeo97@gmail.com>
2023-02-22 08:48:30 +0700

    Add images for test product

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -2,5 +2,6 @@ import logo from './store/logo.png';
 import banner1 from './banner/banner1.png';
 import banner2 from './banner/banner2.png';
 import banner3 from './banner/banner3.png';
+import * as i14prm from './product/14prm';
 
-export { logo, banner1, banner2, banner3 };
+export { logo, banner1, banner2, banner3, i14prm };
---
+++ src/assets/images/product/14prm/index.js
@@ -0,0 +1,10 @@
+import i1 from './1.png';
+import i2 from './2.png';
+import i3 from './3.png';
+import i4 from './4.png';
+import * as black from './black';
+import * as gray from './gray';
+import * as purple from './purple';
+import * as yellow from './yellow';
+
+export { i1, i2, i3, i4, black, gray, purple, yellow };
